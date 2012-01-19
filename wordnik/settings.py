d2a3f4a7676bcb31ff7526b174db33b59db218f1
Thomas Haymore <thomas.haymore@gmail.com>
2012-01-18 20:10:36 -0800

    updated settings

--- wordnik/settings.py
+++ wordnik/settings.py
@@ -45,18 +45,18 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/Users/thaymore/Sites/wordnik/media/'
+MEDIA_ROOT = '/app/wordnik/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://localhost/~thaymore/wordnik/media/'
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/Users/thaymore/Sites/wordnik/static'
+STATIC_ROOT = '/app/wordnik/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
