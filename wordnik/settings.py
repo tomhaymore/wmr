a28b4e53d48c07ae10dd3df8353973f92bff11fb
zeke <zeke@sikelianos.com>
2012-01-20 11:54:39 -0800

    use mysql

--- wordnik/settings.py
+++ wordnik/settings.py
@@ -19,10 +19,10 @@ DATABASES = {
     #    'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     #},
     'default': {
-    	'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'wordnik',                      # Or path to database file if using sqlite3.
-        'USER': 'postgres',                      # Not used with sqlite3.
-        'PASSWORD': 'athena80',                  # Not used with sqlite3.
+    	'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'wmr',                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': '4Bcd3fg',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
