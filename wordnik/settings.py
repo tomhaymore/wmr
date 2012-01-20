b8170d89d7ddb75253233fffd09239578cc06746
zeke <zeke@sikelianos.com>
2012-01-20 10:50:00 -0800

    Added zeke paths to settings.

--- wordnik/settings.py
+++ wordnik/settings.py
@@ -81,6 +81,7 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     '/Users/thaymore/Sites/wordnik/market/static',
+    '/Users/zeke/Projects/wordnik/wmr/wordnik/market/static'
 )
 
 # List of finder classes that know how to find static files in
@@ -116,6 +117,7 @@ TEMPLATE_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     '/Users/thaymore/Sites/wordnik/wordnik/templates',
+    '/Users/zeke/Projects/wordnik/wmr/wordnik/templates'
     #'/app/wordnik/templates',
 )
 
