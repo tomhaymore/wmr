cc5b99e1611602ae8386e41821ddd2dfdae9c441
Thomas Haymore <thomas.haymore@gmail.com>
2012-01-20 13:43:28 -0800

    still deleting bogus routes from urls

--- wordnik/urls.py
+++ wordnik/urls.py
@@ -30,8 +30,6 @@ urlpatterns = patterns('',
     url(r'^report/(?P<report_id>\d+)/$', 'market.views.reportDetail'),
     url(r'^report/all', 'market.views.reportAll'),
     url(r'^login','market.views.login'),
-    #url(r'^admin','market.views.admin'),
-    url(r'^admin/user/add','market.views.userAdd'),
     # url(r'^wordnik/', include('wordnik.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
