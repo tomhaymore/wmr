298f4416a7f72f6bbce5a305f7000a273d29b291
Thomas Haymore <thomas.haymore@gmail.com>
2012-01-20 11:25:36 -0800

    switched default db to postgres

--- wordnik/settings.py
+++ wordnik/settings.py
@@ -10,7 +10,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
+    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'wordnik_db_1',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
@@ -18,7 +18,7 @@ DATABASES = {
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     },
-    'postgres': {
+    'default': {
     	'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'wordnik',                      # Or path to database file if using sqlite3.
         'USER': 'postgres',                      # Not used with sqlite3.
